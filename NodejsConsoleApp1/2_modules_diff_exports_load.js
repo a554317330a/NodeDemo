cfd51b810ae6bfc11fca6fa8ccdbe196d19a0145
wwj <a554317330a@gmail.com>
2017-07-27 17:34:53 +0800

    添加MySql连接文件

---
+++ NodejsConsoleApp1/2_modules_diff_exports_load.js
@@ -0,0 +1,17 @@
+﻿/*
+var Counter = require("./modules_diff_export.js");
+Counter.PrintNextCount();
+*/
+
+/**
+ *  exports => {} <=module.exports，[ if(module.exports === null ? exports : module.exports) ]， ===> module
+ *  同時指向一個内存，如果修改exports的值，并且module是没有值的，那麽module的值也會改變，如果module.exports指向了另外一个内存，那么无论怎么改exports都不改变
+ *  exports 可以用来做变量和方法， module.exports可以用来做对象
+ */
+
+var Counter = require("./modules_diff_export.js");
+module.loaded;
+console.log(module.id);
+console.log(module.filename);
+console.log(module.parent);
+console.log(module.children);
---
+++ NodejsConsoleApp1/connection.js
@@ -0,0 +1,33 @@
+﻿var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: '172.0.0.1',
+    user: 'root',
+    password: '11111',
+    prot : '3306'
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.log('[query] - : ' + err);
+        return;
+    }
+    console.log('[connection connect] - :success ');
+});
+
+connection.query('select 1+1 as solution ', function (err, rows, fields) {
+    if (err)    {
+        console.log('[query] - : ' + err);
+        return;
+    }
+    console.log('the solution is : ' + rows[0].solution);
+});
+
+connection.end(function (err) {
+    if (err) {
+        console.log('[query] - : ' + err);
+        return;
+    }
+    console.log('connection end success');
+});
+
